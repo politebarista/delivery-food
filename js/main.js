5e71a863b6501a02e84ce9d7a3d50278549f873b
politebarista <63463572+politebarista@users.noreply.github.com>
2020-04-11 14:19:47 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 
 basket.addEventListener('click', function(event) {
-    modal.classList.add('is-open')
+    modal.classList.add('is-open');
 } );
 
 close.addEventListener('click', function(event) {
-    modal.classList.remove('is-open')
+    modal.classList.remove('is-open');
 } );
 
 new WOW().init();
