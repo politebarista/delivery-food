e99221cae7926862c9c9becf8057c1272a18c89d
Ilya <63463572+politebarista@users.noreply.github.com>
2020-04-26 14:30:30 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const basket = document.querySelector('#basket');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
+const aut = document.querySelector('#aut');
+const login = document.querySelector('.modal-login');
+const closel = document.querySelector('.closel');
 
 basket.addEventListener('click', function(event) {
     modal.classList.add('is-open');
@@ -8,6 +11,14 @@ basket.addEventListener('click', function(event) {
 
 close.addEventListener('click', function(event) {
     modal.classList.remove('is-open');
+} );
+
+aut.addEventListener('click', function(event) {
+    modal-login.classList.add('login-is-open');
+} );
+
+closel.addEventListener('click', function(event) {
+    modal-login.classList.remove('login-is-open');
 } );
 
 new WOW().init();
